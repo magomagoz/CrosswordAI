554b80884e0f8968656da31854fc533fafd92891
magomagoz <magostinienrico@gmail.com>
2026-03-09 15:02:44 +0100

    Update crosswordai.py

--- crosswordai.py
+++ crosswordai.py
@@ -124,7 +124,7 @@ def main():
             if st.session_state.m.annulla(): st.rerun()
 
     st.divider()
-    st.title("Griglia Cruciverba")
+    st.title("🧩 Griglia Cruciverba")
     
     # Visualizzazione Griglia
     st.markdown(st.session_state.m.render_html(anteprima_data), unsafe_allow_html=True)
